cc722cdaa00bac80bff56018d3ca4dd61a5931a6
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-07-14 02:38:41 +0200

    n/a

--- colored-output.js
+++ colored-output.js
@@ -6,6 +6,16 @@
 //
 //   Or... compile program that is in stackoverflow thread
 //
+//      See also https://superuser.com/questions/413073/windows-console-with-ansi-colors-handling/1300251#1300251
 //
+//
+
+
+//
+//  Following two lines: workaround if registry was not changed.
+//
+var ps = WScript.CreateObject("WScript.Shell").Exec("powershell.exe -nop -ep Bypass -c \"exit\"");
+while (ps.Status == 0) WScript.Sleep(50);
+
 WScript.Echo('\x1B[7mReverse\x1B[0m Normal')
 WScript.stdout.WriteLine('\x1B[7mReverse\x1B[0m Normal')
--- colored-output.vbs
+++ colored-output.vbs
@@ -9,5 +9,19 @@
 '      See also https://superuser.com/questions/413073/windows-console-with-ansi-colors-handling/1300251#1300251
 '
 
+option explicit
+
+
+'
+'   Workaraound if registry was not changed.
+'
+dim ps
+set ps = CreateObject("WScript.Shell").Exec("powershell.exe -nop -ep Bypass -c ""exit""")
+while ps.Status = 0
+      wScript.sleep 50
+wend
+
+
 WScript.Echo("[7mReverse[0m Normal")
 WScript.stdout.WriteLine("[7mReverse[0m Normal")
+WScript.Echo("[1;31mRed[0m Normal")
